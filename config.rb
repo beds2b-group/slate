74b74bba7cb72ee32212fdc9f11b719f5d048b93
fplaza-shr <fplaza@senatorhr.com>
2023-04-20 11:37:15 +0200

    force upload

--- config.rb
+++ config.rb
@@ -39,6 +39,7 @@ end
 activate :relative_assets
 set :relative_links, true
 
+
 # Build Configuration
 configure :build do
   # We do want to hash woff and woff2 as there's a bug where woff2 will use
